8f7634efcb3a165a66200ab6022c51176914575d
Clebson Santos <clebsonjump@gmail.com>
2023-10-13 13:35:39 -0300

    Start Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function hellowWord(){
+
+        console.log("Hello World");
+}
+
+const saudacao = () =>{
+    var data = new Date();
+    return data.getHours <=12? "Bom dia": data.getHours() <=18? "Boa Tarde": "Boa Noite";
+}
+
+hellowWord();
+
+console.log(`A saudação do momento é: ` + saudacao());
